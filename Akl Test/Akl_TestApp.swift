191aaf883f248a52557e22fae429016be91e5b2d
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2023-05-12 00:20:56 +1200

    init commit

---
+++ Akl Test/Akl_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Akl_TestApp.swift
+//  Akl Test
+//
+//  Created by Ilia Sidorenko on 12/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Akl_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
